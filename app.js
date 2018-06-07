948f20326912b8b3ae6f5428d8a34b8c88d658c6
gharam37 <g_z48@yahoo.com>
2018-06-08 00:09:47 +0200

    indented , documented , cleaned , removed unncessary comments from sources

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-/*
-  Aman Kharbanda
-  Subscribe my channel for more videos
-  https://goo.gl/H91NRo
-  Thanks!
-*/
-
 const express = require('express');
 var app = express();
 var upload = require('express-fileupload');
 var bodyParser = require('body-parser')
 const http = require('http');
-var ejs = require('ejs');
 var port = Number(process.env.PORT || 3000)
-http.Server(app).listen(port); // make server listen on port 80
-var success='No'
+http.Server(app).listen(port); // make server listen on port 3000
+var fs = require('fs');
+var util = require('util')
 var csv=null
-
-app.use(upload()); // configure middleware
+app.use(upload()); 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 console.log("Server Started at port 3000");
 
@@ -24,46 +16,28 @@ app.set("view engine","ejs")
 
 
 app.get('/',function(req,res){
-  res.render('view.ejs',{ title: 'Your output Shows here'  ,warn:'' });
-
-
-
-
+  res.render('view.ejs',{ Output: 'Your output Shows here'  ,warn:'' });
 })
 
 app.get('/download',function(req,res){
-  var fs = require('fs');
 if (fs.existsSync(__dirname + '/views/output.csv')) {
 
   res.download(__dirname + '/views/output.csv', 'csvfile.csv', function(err){
   if (err) {
     console.log(err);
-  } else {
   }
 });
 }
 else{
-
-  res.render('view.ejs',{ title: 'Your output shows here'  ,warn:'Upload the file first' });
-
-
+  res.render('view.ejs',{ Output: 'Your output shows here'  ,warn:'Upload the file first' });
 }
 
-
-
-
-
-
-
-
 })
-
+//Remove file in server if any ..Convert the String and Make it downloadable
 app.post('/',urlencodedParser,function(req,res){
-  var fs = require('fs')
-
 
   fs.stat('./views/output.csv', function (err, stats) {
-console.log(stats);//here we got all information of file in stats variable
+  console.log(stats);//here we got all information of file in stats variable
 
 if (err) {
     return console.error(err);
@@ -80,14 +54,12 @@ fs.unlink('./views/output.csv',function(err){
     jsonexport(JsonData,function(err, csv){
     if(err) return console.log(err);
      console.log(csv);
-     var fs = require('fs')
-     var util = require('util')
      fs.writeFileSync('views/output.csv', csv, 'utf-8')
-     res.render('view.ejs',{ title: csv ,warn:''});
+     res.render('view.ejs',{ Output: csv ,warn:''});
 
    })
     } catch(e) {
-      res.render('view.ejs',{ title: 'Wrong Json Format' ,warn:''});
+      res.render('view.ejs',{ Output: 'Wrong Json Format' ,warn:''});
 
         alert(e); // error in the above string (in this case, yes)!
     }
@@ -95,13 +67,12 @@ fs.unlink('./views/output.csv',function(err){
 
 })
 
-
+//Redirect to Download .. Remove File in Server then Add a new one And
 app.post('/download',urlencodedParser,function(req,res){
-  var fs = require('fs')
 
 
   fs.stat('./views/output.csv', function (err, stats) {
-console.log(stats);//here we got all information of file in stats variable
+  console.log(stats);//here we got all information of file in stats variable
 
 if (err) {
     return console.error(err);
@@ -114,10 +85,8 @@ fs.unlink('./views/output.csv',function(err){
 });
 
   if(req.files.upfile){
-    var file = req.files.upfile,
-      name = file.name,
-      type = file.mimetype;
-      data = file.data;
+    var file = req.files.upfile;
+    var data = file.data;
       try {
       var JsonData = JSON.parse('['+data+']');
       var jsonexport = require('jsonexport');
@@ -125,17 +94,15 @@ fs.unlink('./views/output.csv',function(err){
       jsonexport(JsonData,function(err, csv){
       if(err) return console.log(err);
        console.log(csv);
-       var fs = require('fs')
-       var util = require('util')
        fs.writeFileSync('views/output.csv', csv, 'utf-8')
 
-       res.render('view.ejs',{ title: csv,warn:'Uploaded' });
+       res.render('view.ejs',{ Output: csv,warn:'Uploaded' });
 
      })
    }
 
    catch(e) {
-     res.render('view.ejs',{ title:'Your output shows here', warn:'File is not in Json format'});
+     res.render('view.ejs',{ Output:'Your output shows here', warn:'File is not in Json format'});
 
        alert(e); // error in the above string (in this case, yes)!
    }
@@ -144,14 +111,14 @@ fs.unlink('./views/output.csv',function(err){
 
   }
   else if(req.body.hasOwnProperty('download')) {
-    res.render('view.ejs', {title:'Your output Shows here ',warn:'Downloaded latest converted file' });
+    res.render('view.ejs', {Output:'Your output Shows here ',warn:'Downloaded latest converted file' });
 
 
 
     res.end();
   }
   else if(req.body.hasOwnProperty('upload')) {
-    res.render('view.ejs', {title:'Your output Shows here ',warn:'Upload the file first' });
+    res.render('view.ejs', {Output:'Your output Shows here ',warn:'Upload the file first' });
     console.log('Here');
 
     res.end();
